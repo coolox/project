9f702040853f6da18054286a2a2045e4cf2114d5
coolox <coolox@gmail.com>
2022-11-23 14:44:18 +0300

    fetch added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 function Homepage() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => setData(json))
+  }, [])
+  console.log(data)
   return <h1>Home</h1>
 }
 export default Homepage
