fc0da98830123012e363218193b4a53867c8880d
coolox <coolox@gmail.com>
2022-11-24 11:09:09 +0300

    local data used

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 function Homepage() {
-  return <h1>Home</h1>
+  const url = './mock/data.json'
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(e => console.error(e))
+  }, [])
+
+  console.log(data)
+
+  return (
+    <>
+      <h1>Home Page</h1>
+    </>
+  )
 }
 export default Homepage
