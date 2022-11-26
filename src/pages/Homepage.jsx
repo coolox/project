eeb8896363998b0b51bc20cda27a846ca8d8bd96
coolox <coolox@gmail.com>
2022-11-26 03:50:55 +0300

    state get data as array of objects

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,12 +7,19 @@ function Homepage() {
   useEffect(() => {
     fetch(url)
       .then(response => response.json())
-      .then(json => setData(json))
+      .then(data => {
+        const accum = []
+        for (let [key, value] of Object.entries(data)) {
+          value.id = key
+          accum.push(value)
+        }
+        return accum
+      })
+      .then(res => setData(res))
       .catch(e => console.error(e))
   }, [])
 
   console.log(data)
-
   return (
     <>
       <h1>Home Page</h1>
